0977466f084ef015e9df51c85b162c0df8c7d7aa
abcoden <abcoden@proton.me>
2024-07-04 23:45:03 +0200

    add first texts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,44 @@
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
         <h1 class="text-h2 font-weight-bold">SOPROS</h1>
+
+      </div>
+      <div class="text-left text-body-3 font-weight-light">
+        <div class="mt-2">This Self-Assessment Tool was developed within the
+          EU-funded Erasmus+ Project “Assessing, Evaluating and Implementing Athletes' Social Protection in Olympic
+          Sports – SOPROS”. The project’s aim is to assess and evaluate the current social protection situation of elite
+          athletes
+          in Europe.</div>
+        <div class="mt-2">Learn more about the SOPROS Project here (TODO: where Link).</div>
+        <div class="mt-2">Social protection affects our daily lives. Yet, we are often
+          not aware of the details of our individual
+          situation, nor about our actual social protection entitlements. The Tool offers you the possibility to
+          self-assess your current social protection situation as an elite athlete in your country.
+        </div>
+
+        <h3 class="text-h5 font-weight-bold mt-5 text-center">Social Protection</h3>
+
+        <div class="mt-2">According to the International Labour Organization and the
+          European Union, social protection is a human right. You can learn more about social protection here (TODO:
+          where Link).</div>
+        <div class="mt-2">As an elite athlete, your social protection situation is
+          determined by two components:</div>
+
+        <h4 class="text-h7 font-weight-bold mt-5 ml-5">Statutory provisions</h4>
+        <div class="mt-2">The national laws of your country determine your social protection situation. These provisions
+          <u>depend on your social status</u> under national law, for example as a resident, employee, self-employed
+          person, or
+          elite athlete. International organisations have developed a catalogue of 10 statutory provisions of social
+          protection to which everyone should have access.
+        </div>
+
+        <h4 class="text-h7 font-weight-bold mt-5 ml-5">Athlete-specific measures</h4>
+        <div class="mt-2">Governments and private sport organisations develop policies that <u>only apply to elite
+            athletes</u>. These measures may depend on your membership in a national team or cadre, and your personal
+          contractual relationships with the stakeholders running elite sport programmes. Athlete-specific measures
+          include a specific accident or injury insurance, pregnancy and maternity/paternity policies, specific athlete
+          pensions or retirement funds and other measures that are in place to protect you against specific risks
+          associated with your sporting practice and changing life circumstances.</div>
       </div>
 
       <div class="py-4" />
@@ -21,6 +59,16 @@
             <template #title>
               <h2 class="text-h5 font-weight-bold">Select country</h2>
             </template>
+            <div class="mr-5 ml-5">
+              Please select the country which you represent as an elite athlete. The Tool asks you some simple
+              questions which refer to your social status and your membership(s) as an elite athlete. Based on the
+              information you provide; an algorithm calculates your personal social protection situation.
+            </div>
+
+            <div class="mr-5 ml-5 mt-3 mb-3">
+              Before submitting your data for analysis, please verify its correctness and read our data protection
+              policy. To submit your data, simply click the button “NAME of the button” (TODO: name Button).
+            </div>
 
             <v-select v-model="selected_country" :items="countries" item-title="name" item-value="id" label="Select"
               persistent-hint return-object single-line></v-select>
@@ -59,7 +107,31 @@
         </v-col>
       </v-row>
 
-      <v-row>
+      <div class="text-left text-body-3 font-weight-light">
+        <h3 class="text-h5 font-weight-bold mt-5 text-center">Filling in the Athlete-Survey</h3>
+        <div class="mt-2">
+          After having read your personal results, you have the chance to use our Survey to paint an even more
+          comprehensive
+          picture about your social protection situation and to share your personal views about it. Through your
+          participation
+          in our Survey, you contribute to the identification of existing gaps and the development of new social
+          protection
+          measures. Ultimately, you support the project team’s endeavour to improve athletes’ welfare in your country
+          and
+          beyond.
+        </div>
+
+        <h3 class="text-h5 font-weight-bold mt-5 text-center">Disclaimer</h3>
+        <div class="mt-2">
+          Please note that the output of the Tools does not offer legal counsel. Your entitlements to certain provisions
+          and
+          measures may depend on individual factors which cannot be captured by the Tool. Professional counsel and
+          advise may
+          be required.
+        </div>
+      </div>
+
+      <v-row class="mt-5">
         <v-col cols=" 12">
           <v-card class="py-4" color="surface-variant" append-icon="mdi-calculator" rounded="lg" variant="outlined">
             <template #image>
@@ -97,6 +169,7 @@
           </v-card>
         </v-col>
       </v-row>
+
     </v-responsive>
   </v-container>
 </template>
